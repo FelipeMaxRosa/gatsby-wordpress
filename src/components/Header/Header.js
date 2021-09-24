5d5f72b9ae127f7666d688e486b04bc220179b37
Felipe Maximiliano da Rosa <90468812+frosa-fullstacklabs@users.noreply.github.com>
2021-09-23 21:35:21 -0300

    feat: navigation menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "gatsby";
 import Logo from "../../images/logo.svg";
+import Navigation from "../Navigation/Navigation";
 import { useMenuQuery } from "../../hooks/useMenuQuery";
 import { Wrapper, Content } from "./Header.styles";
 
 const Header = () => {
-  const { site, wpMenu } = useMenuQuery();
-  console.log({ site, wpMenu });
+  const { site, menu } = useMenuQuery();
+  console.log({ site, menu });
 
   return (
     <Wrapper>
@@ -14,6 +15,8 @@ const Header = () => {
         <Link to="/">
           <img src={Logo} alt={site.siteMetadata.title} />
         </Link>
+
+        <Navigation menu={menu.menuItems.nodes} />
       </Content>
     </Wrapper>
   );

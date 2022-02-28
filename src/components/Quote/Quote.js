4e4221edbcf62c4c8f846ee459e9840126b9099e
felipemaxrosa <felipemax.suporteti@gmail.com>
2022-02-28 14:00:04 -0300

    feat: Index - Quote

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useQuoteQuery } from "../../hooks/useQuoteQuery";
+import QuoteImg from "../../images/quote.svg";
+import { Wrapper, Content } from "./Quote.styles";
 
 const Quote = () => {
-  return <div>Quote</div>;
+  const data = useQuoteQuery();
+  const header = data.wpPage.ACF_HomePage.citat1Text;
+  const author = data.wpPage.ACF_HomePage.citat1Author;
+
+  return (
+    <Wrapper>
+      <Content>
+        <img src={QuoteImg} alt="quote" />
+        <h6>{header}</h6>
+        <p>{author}</p>
+      </Content>
+    </Wrapper>
+  );
 };
 
 export default Quote;
